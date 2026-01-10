019747fe1b772196c8e76506c0efa41451ad4279
AdrianGrs <51717231+AdrianGrs@users.noreply.github.com>
2026-01-10 02:21:51 +0000

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,2 +1,4 @@
-import { callback } from '@openlab/vercel-netlify-cms-github';
-export default function handler(req, res) { return callback(req, res); }
+import { callback } from "@openlab/vercel-netlify-cms-github";
+export default function handler(req, res) {
+  return callback(req, res);
+}
